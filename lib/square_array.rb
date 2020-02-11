3297ce780d874420a3d692ee3cf6eb4425f15c68
Brett Bedevian <brettbedevian@gmail.com>
2020-02-11 15:34:06 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   counter = 0 
   new_numbers = []
   while counter < array.length do
-    square = (array[counter])**
+    square = (array[counter])**2
     new_numbers.push(square)
     counter += 1
 end
